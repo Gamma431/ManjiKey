bc17cfbf026dfc638268c31eef36ea99dd540591
Arman Ghazaryan <ghazaryanarman843@gmail.com>
2026-05-06 10:22:52 +0400

    footer + logo(changed)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
 export default function Footer() {
   return (
-    <div className="w-[96%] h-[65vh] mx-[2%] text-white bg-violet-950 rounded-[25px] flex ">
-      
+    <div className="w-[96%] h-[65vh] mx-[2%] text-white bg-gray-800 rounded-[25px] flex p-[1%]">
+      <div className="w-[50%] h-full ">
+        <div className="w-[100%] h-[40%] bg-[url('/img/Logo/no-bg-logo.png')] bg-cover bg-center"></div>
+        <div>
+        
+        </div>
+      </div>
+      <hr className="mx-[2%] h-full border border-gray-400 rounded-[5px]"/>
+      <div>
+
+      </div>
     </div>
   )
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom"
 export default function Nav() {
   return (
     <div className="w-[100%] bg-gray-950 h-20 flex justify-between p-[1%] items-center">
-      <div className="w-[50%] h-[100%] flex items-center text-xl object-cover gap-[2%]">
+      <div className="w-[50%] h-full flex items-center text-xl object-cover">
         <div className="w-[25%] h-full bg-[url('/img/Logo/no-bg-logo.png')] bg-cover bg-no-repeat bg-center"></div>
+         <img src='public/img/Logo/no-bg-logo.png'/>
       </div>
       <div className="w-[25%] flex text-white gap-[10%] justify-center">
           <Link to="/">Home</Link> 
