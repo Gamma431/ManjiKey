082b54874da463369929eb4aed4b3b8d2d98c1a0
Arman Ghazaryan <ghazaryanarman843@gmail.com>
2026-05-08 18:32:32 +0400

    About page edited

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,7 +1,18 @@
+import {motion} from "motion/react";
+
 export default function About() {
   return (
-    <div>
-      About page
-    </div>
+    <motion.div className="p-[2%] w-full h-auto">
+        <div className="w-full px-[1%] h-[180vh] flex flex-col items-center justify-start bg-gray-900 rounded-2xl">
+            <h1 className="text-white text-[300px]  def-font"> Lets Key  </h1>
+            <p className=" text-white text-[25px] def-font">For the last Time</p>
+            <div className="w-full h-[70vh] bg-gray-950 rounded-2xl p-[2%] flex justify-center gap-[2%]">
+              <motion.img whileHover={{scale:"1.08", boxShadow:"10px 10px 50px black"}} src="public/img/lets-key.png" alt="" className="duration-300 w-[40%] rounded-[140px]"/>
+              <p className="w-[50%] text-[30px] p-[5%] text-white ">We're making best way to give you a cahnce have a nice keyboard and type on it as its your best one you ever had, lately We're about to start selling Keycaps and mechaninc & magnetic swithves too, Soo its going to be sooner and sooner keep following and get more items to have our <i className="underline decoration-1 text-[35px]">limited</i> Edition <strong>Keycaps</strong>  </p>
+            </div>
+
+        </div>
+
+    </motion.div>
   )
 }
--- src/components/MiniSideMenu.tsx
+++ src/components/MiniSideMenu.tsx
@@ -2,7 +2,7 @@ import { motion } from "motion/react"
 import {Link} from "react-router-dom";
 export default function MiniSideMenu() {
     return (
-        <motion.div whileHover={{width:"5vw", left:"94vw",fontSize:"16px"}} className="left-[98vw] text-[0px] bg-gray-800 w-[1%] h-[22vh] z-1000  fixed bottom-0 top-[50vh] flex flex-col items-center justify-center py-1 rounded-3xl text-white ">
+        <motion.div whileHover={{width:"5vw",height:"22vh", left:"94vw",fontSize:"16px"}} className="left-[98vw] text-[0px] bg-gray-800 w-[1%] h-[10vh] z-1000  fixed bottom-0 top-[50vh] flex flex-col items-center justify-center py-1 rounded-3xl text-white ">
            <div className=" flex p-2  w-[90%] h-full flex-col items-center justify-center gap-1">
                <motion.div whileHover={{scale:"1.1", }} className="bg-gray-700 w-full h-[33%] rounded-xl flex flex-col items-center justify-center">
                   <Link to="/" >Home</Link>
